ece78c5cd949e4573bba2cbbb7cc20ae11ac0275
Eric Carty-Fickes <ericcf@northwestern.edu>
2014-07-23 16:06:16 -0500

    exploratory development

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # Anthracite JavaScript Client
 
 A client library for accessing the [Anthracite](https://github.com/cbitstech/anthracite) API.
+
+## Dependencies
+
+None
+
+## Browser compatibility
+
+Due to use of ```XMLHttpRequest``` and ```Date.prototype.toISOString```, some older browsers are excluded.
+
+| Chrome | Firefox | IE  | Opera | Safari | iOS Safari | Android Browser |
+| 21+    | 21+     | 10+ | 12.1+ | 5.1+   | 6.0+       | 4+              |
---
+++ anthracite.js
@@ -0,0 +1,60 @@
+;(function() {
+  function A(options) {
+    options = options || {};
+
+    this._serverUrl = options.serverUrl || "http://localhost:3000";
+  }
+
+  // Public methods
+
+  A.prototype.logEvent = function() {
+    var aEvent = new AnthraciteEvent({
+      kind: 'app_crash',
+      payload: 'some JSON',
+      sharedSecret: 'default',
+      userId: 'test',
+      userAgent: 'web'
+    });
+    this._postEvent(aEvent);
+
+    return this;
+  };
+
+  // Private methods
+
+  A.prototype._postEvent = function(aEvent) {
+    var httpRequest = new XMLHttpRequest();
+    var isAsynchronous = true;
+
+    httpRequest.open("POST", this._serverUrl + "/events", isAsynchronous);
+    httpRequest.setRequestHeader("Content-Type", "application/json");
+    httpRequest.send(aEvent.toJson());
+  };
+
+  // Supporting classes
+
+  function AnthraciteEvent(options) {
+    options = options || {};
+
+    this.kind = options.kind || 'default_event';
+    this.payload = options.payload || 'NO DATA';
+    this.sharedSecret = options.sharedSecret || 'default';
+    this.userId = options.userId || 'test';
+    this.userAgent = options.userAgent || 'web';
+  }
+
+  AnthraciteEvent.prototype.toJson = function() {
+    return JSON.stringify({
+      event: {
+        kind: this.kind,
+        date_emitted: (new Date()).toISOString(),
+        payload: this.payload,
+        shared_secret: this.sharedSecret,
+        user_ID: this.userId,
+        user_agent: this.userAgent
+      }
+    });
+  };
+
+  window.Anthracite = A;
+}.call(this));
